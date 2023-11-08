13054acffafc46ed5bce052896636b2e9a265a2b
Jona <llamasjonat@gmail.com>
2023-11-08 18:27:40 -0300

    fix(page): changed animation for sad face on Deposit

--- src/app/[lng]/deposit/components/CashDeposit.tsx
+++ src/app/[lng]/deposit/components/CashDeposit.tsx
@@ -2,14 +2,15 @@ import React from 'react'
 
 import { Flex, Heading, Divider, Text } from '@/components/UI'
 import Animations from '@/components/Animations'
-import Wifi from '@/components/Animations/wifi.json'
+import SadFace from '@/components/Animations/sad-face.json'
 import Container from '@/components/Layout/Container'
 
 const CashDeposit = () => {
   return (
     <Container size="small">
       <Flex direction="column" justify="center" align="center" flex={1}>
-        <Animations data={Wifi} />
+        <Animations data={SadFace} />
+        <Divider y={16} />
         <Heading as="h4">¿Tenés solo efectivo?</Heading>
         <Divider y={4} />
         <Text size="small" align="center">
