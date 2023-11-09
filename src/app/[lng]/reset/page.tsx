f6dfdc9c893b7f8dfc556ef1708d9b123674af80
ferpa <feririarte.1996@gmail.com>
2023-11-09 09:59:33 -0300

    add reset card claim endpoint

--- src/app/[lng]/reset/page.tsx
+++ src/app/[lng]/reset/page.tsx
@@ -1,19 +1,19 @@
 'use client'
-import Logo from '@/components/Logo'
 import Container from '@/components/Layout/Container'
 import { Loader } from '@/components/Loader/Loader'
+import Logo from '@/components/Logo'
 
-import { Divider, Flex, Heading, Text } from '@/components/UI'
+import { Flex, Heading, Text } from '@/components/UI'
+import { LAWALLET_VERSION } from '@/constants/constants'
 import { LaWalletContext } from '@/context/LaWalletContext'
 import { useTranslation } from '@/hooks/useTranslations'
-import { requestCardActivation } from '@/interceptors/card'
+import { restartCardClaim } from '@/interceptors/card'
 import { generateUserIdentity } from '@/interceptors/identity'
 import { cardActivationEvent } from '@/lib/events'
+import theme from '@/styles/theme'
 import { NostrEvent } from '@nostr-dev-kit/ndk'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { useContext, useEffect } from 'react'
-import { LAWALLET_VERSION } from '@/constants/constants'
-import theme from '@/styles/theme'
 
 export default function Page() {
   const { t } = useTranslation()
@@ -32,14 +32,13 @@ export default function Page() {
     const username: string = 'test'
     generateUserIdentity(recoveryNonce, username).then(generatedIdentity => {
       setUserIdentity(generatedIdentity).then(() => {
-        // console.log(generatedIdentity)
-        // cardActivationEvent(recoveryNonce, generatedIdentity.privateKey)
-        //   .then((cardEvent: NostrEvent) => {
-        //     requestCardActivation(cardEvent).then(() => {
-        //       router.push('/dashboard')
-        //     })
-        //   })
-        //   .catch(() => router.push('/'))
+        cardActivationEvent(recoveryNonce, generatedIdentity.privateKey)
+          .then((cardEvent: NostrEvent) => {
+            restartCardClaim(cardEvent).then(() => {
+              router.push('/dashboard')
+            })
+          })
+          .catch(() => router.push('/'))
       })
     })
   }, [])
