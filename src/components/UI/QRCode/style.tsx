f5f3038512988de9898ca2bdfde5b8985e9c4ae3
Jona <llamasjonat@gmail.com>
2023-12-01 22:58:01 -0300

    fix(component): base toast integrated on QRCode

--- src/components/UI/QRCode/style.tsx
+++ src/components/UI/QRCode/style.tsx
@@ -4,19 +4,61 @@ import { styled } from 'styled-components'
 
 import theme from '@/styles/theme'
 
-interface QRCodeProps {
-  $isSmall: boolean
-}
+interface QRCodeProps {}
 
 export const QRCode = styled.div<QRCodeProps>`
+  position: relative;
+
   display: flex;
-  flex: 1;
   justify-content: center;
   align-items: center;
-  width: 100%;
-  height: 100%;
-
-  padding: ${props => (props.$isSmall ? '16px 0' : '32px 0')};
+  width: 200px;
+  height: 200px;
 
   background-color: ${theme.colors.white};
+  border-radius: 12px;
+
+  cursor: pointer;
+`
+
+interface ToastProps {
+  $isShow: boolean
+}
+
+export const Toast = styled.div<ToastProps>`
+  position: absolute;
+  bottom: 100%;
+  margin-bottom: 5px;
+  opacity: ${props => (props.$isShow ? 1 : 0)};
+  z-index: 1;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 28px;
+
+  padding: 0 8px;
+
+  background-color: ${theme.colors.primary15};
+  border-radius: 12px;
+
+  color: ${theme.colors.primary};
+
+  div {
+    position: relative;
+  }
+
+  span {
+    position: absolute;
+    top: 100%;
+    z-index: 1;
+
+    width: 0;
+    height: 0;
+
+    border-right: 5px solid transparent;
+    border-bottom: 5px solid transparent;
+    border-left: 5px solid transparent;
+    border-top: 5px solid ${theme.colors.primary15};
+  }
 `
