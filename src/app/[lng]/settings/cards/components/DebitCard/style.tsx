d1ec26faf9b383a18936aaffa2627e860720ad96
Jona <llamasjonat@gmail.com>
2023-12-19 07:21:40 -0300

    style(component): removed shadow on active on CardImage

--- src/app/[lng]/settings/cards/components/DebitCard/style.tsx
+++ src/app/[lng]/settings/cards/components/DebitCard/style.tsx
@@ -9,11 +9,15 @@ export const CardImage = styled.div<CardImageProp>`
   display: flex;
 
   border-radius: 12px;
-  box-shadow: ${props =>
-    props.$isActive ? `0 18px 30px 0 ${theme.colors.primary}33}` : 'none'};
+  /* box-shadow: ${props =>
+    props.$isActive ? `0 18px 30px 0 ${theme.colors.primary}33}` : 'none'}; */
 `
 
 export const ConfigCard = styled.div<CardImageProp>`
   opacity: ${props => (props.$isActive ? '1' : 0)};
   width: ${props => (props.$isActive ? 'auto' : 0)};
+
+  padding: 0 20px;
+
+  transition-duration: 0.4s;
 `
